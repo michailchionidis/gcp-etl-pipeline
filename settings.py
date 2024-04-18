53e1ec7bbd07d2d3c78d96995e515e2f98a2ac7c
Chionidis Michail <104796421+michailchionidis@users.noreply.github.com>
2024-04-18 14:39:08 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+is_local_environment = True # Set to False before deploying to Cloud Functions
+cloud_storage_service_account = "" # Replace with your Cloud Storage service account filename
+bigquery_service_account = "" # Replace with your BigQuery service account filename
+
+api_key = '' # Replace with your OpenWeather API key
+project_id = '' # Replace with your GCP Project ID
+dataset_id= '' # Replace with your BigQuery Dataset Name
+
